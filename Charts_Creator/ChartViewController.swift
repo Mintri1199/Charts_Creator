c2d01373a48b0b8cb7ca0518257ee27b057b7966
Mintri1199 <Mintri1199@github.com>
2018-10-18 16:19:46 -0700

    Allow data to pass by singleton

--- Charts_Creator/ChartViewController.swift
+++ Charts_Creator/ChartViewController.swift
@@ -15,31 +15,64 @@ class ChartViewController: UIViewController {
     @IBOutlet weak var pieChart: PieChartView!
     
     // set the global variables for data
-    var firstValue = PieChartDataEntry(value: 10)
-    var secondValue = PieChartDataEntry(value: 34)
+    // var firstValue = PieChartDataEntry(value: 10)
+    // var secondValue = PieChartDataEntry(value: 34)
     
+    // empty list of values
     var listOfValues = [PieChartDataEntry]()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        pieChart.chartDescription?.text = "test"
-   
-        firstValue.label = "First"
+    // empty list of colors
+    var colors = [UIColor]()
+    
+    func appendingListOfData () -> [PieChartDataEntry]{
+        var listOfData = DataController.dataArray
+        var getterList = [PieChartDataEntry]()
         
-        secondValue.label = "Second"
-        
-        listOfValues = [firstValue, secondValue]
-        
-        updateCharts()
+        for object in listOfData{
+
+            getterList.append(PieChartDataEntry(value: object.1))
+        }
+        return getterList
     }
     
+    func addingLabels() {
+        var index = 0
+        
+        for entry in listOfValues{
+            entry.label = DataController.dataArray[index].0
+            index += 1
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        // append the data from datacontroller into an Array of PieChartEntry values
+        listOfValues = appendingListOfData()
+        print("Count: \(listOfValues.count)")
+        
+        
+        print(listOfValues)
+        // Add labels to the values PieChartEntry list
+        addingLabels()
+        print(listOfValues)
+        updateCharts()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func addingColor() {
+        var index = 0
+        
+        for _ in listOfValues{
+            colors.append(DataController.dataArray[index].2)
+            index += 1
+        }
+    }
 
     func updateCharts(){
         let chartDataSet = PieChartDataSet(values: listOfValues, label: "")
         let chartData = PieChartData(dataSet: chartDataSet)
-        
-        let colors = [UIColor.red, UIColor.cyan]
-        chartDataSet.colors = colors as! [NSUIColor]
+        let colorList = [UIColor.red, UIColor.cyan]
+        chartDataSet.colors = colorList as! [NSUIColor]//colors as [NSUIColor]
         
         pieChart.data = chartData
         
